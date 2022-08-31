e69ff5809255ffd8ae7e85e10958222dbeb6eaec
Sorawid Mingmolee <bossxlp@gmail.com>
2022-08-31 19:43:02 +0700

    first push

---
+++ TestThreeFive.js
@@ -0,0 +1,12 @@
+for (let i=1; i<=100; i++){
+    if(i%3===0 && i%5===0){
+        console.log("ThreeFive") 
+    }else if(i%3===0){
+        console.log("Three") 
+    }else if (i%5===0){
+        console.log("Five") 
+    }else{
+        console.log(i)
+    }
+}
+
